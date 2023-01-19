699d8f6166e1a7784eb3d82565cf66fbe6fb4d8b
sudhi777 <87425107+sudhi7seas@users.noreply.github.com>
2023-01-19 13:12:19 +0000

    Remove the last character from a csv file and save the contents to a new csv file.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 with open('file3.csv', 'r') as file:
   content = file.read()
 
-#Get all the characters except the last one. Using this slicing operation.
-#This slicing is suitable when the file contains strings.
+#Get all the characters except the last one. Use the slicing operation.
+#The slicing operation is suitable when the file contains strings.
 modified_content = (content[:-1])
 
 with open('file3_modified.csv', 'w') as file:
